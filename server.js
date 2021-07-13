32d99dfa8086d32f2700f55877d8169443a9c812
Sai sirisha maddula <saisirisham@iquadra.com>
2021-07-13 10:03:09 -0400

    updated PATH in server.js

--- server.js
+++ server.js
@@ -6,11 +6,18 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/widget-store-web'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/widget-store-web/index.html'));
 });
 
+console.log('PATH-->',__dirname);
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
+
+
